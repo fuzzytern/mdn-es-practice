1f7e7201db1a4d59044ea46821d3bec592f9b41e
Nicolas Wormser <nicolas.wormser@gmail.com>
2016-10-27 21:01:48 +0400

    Add some object and array related exercises.

--- grammar-and-types/var-let-const-example.js
+++ grammar-and-types/var-let-const-example.js
@@ -13,10 +13,10 @@ var exercise0 = function () {
     var v = 2;
   }
 
-  var answer = 2; // TODO - Replace undefined with the correct value of `v` (1 or 2)
+  var result = 2; // TODO - Replace undefined with the correct value of `v` (1 or 2)
 
   // No changes below that point
-  console.log(`[${(v === answer ? "PASS" : "FAIL")}] - Exercise 0`);
+  console.log(`[${(v === result ? "PASS" : "FAIL")}] - Exercise 0`);
 
 }
 
@@ -29,10 +29,10 @@ var exercise1 = function () {
     let v = 2;
   }
 
-  var answer = 1; // TODO - Replace undefined with the correct value of `v` (1 or 2)
+  var result = 1; // TODO - Replace undefined with the correct value of `v` (1 or 2)
 
   // No changes below that point
-  console.log(`[${(v === answer ? "PASS" : "FAIL")}] - Exercise 1`);
+  console.log(`[${(v === result ? "PASS" : "FAIL")}] - Exercise 1`);
 
 }
 
@@ -55,7 +55,42 @@ var exercise2 = function () {
 
 }
 
+// - Exercise 3 - Objects & const
+var exercise3 = function () {
+  // TODO - Perform any change required to this code so that it doesn't raise "TypeError: Assignment to constant variable."
+  // TODO - Refactor this code.
+  const obj = {one: "one", two: "two"};
+  obj.one = 1;
+  obj.two = 2;
+  obj = Object.assign(obj, {three: 3});
+
+  let result = obj;
+
+  // No changes below that point
+  let condition = (JSON.stringify(obj) === JSON.stringify({one: 1, two: 2, three: 3}));
+  console.log(`[${( condition ? "PASS" : "FAIL")}] - Exercise 3`);
+}
+
+// - Exercise 4 - Objects & const
+var exercise4 = function () {
+  // TODO - Identify lines potentially raising TypeError exceptions and fix them
+  // TODO - Refactor this code (use only one variable).
+  const arr = ["zero","one","two"]
+  arr[0] = 0;
+  arr[1] = 1;
+  arr[2] = 2;
+  arr = arr.concat([3])
+
+  let result = arr;
+
+  // No changes below that point
+  let condition = (JSON.stringify(arr) == '[0,1,2,3]');
+  console.log(`[${( condition ? "PASS" : "FAIL")}] - Exercise 3`);
+}
+
 // TODO - Uncomment the function calls as you pass the test
 exercise0();
 exercise1();
 exercise2();
+exercise3();
+exercise4();
